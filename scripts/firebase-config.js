5e493174bdca5ced9da553c565b2b007a7adfb89
nimon10 <n300403@gmail.com>
2025-02-28 17:07:08 -0500

    Actualización de archivos de ventas y configuración de Firebase

--- scripts/firebase-config.js
+++ scripts/firebase-config.js
@@ -3,18 +3,16 @@ import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebas
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
 import { getAnalytics } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-analytics.js";
 
-// Tu configuración de Firebase (corregida)
 const firebaseConfig = {
   apiKey: "AIzaSyCrGcoLW3tEtrFGlNnX7NIbd1RfOOhaVwY",
   authDomain: "sdf1-34631.firebaseapp.com",
   projectId: "sdf1-34631",
-  storageBucket: "sdf1-34631.appspot.com", // 🔹 Corregido
+  storageBucket: "sdf1-34631.appspot.com",
   messagingSenderId: "122107622167",
   appId: "1:122107622167:web:ad96d0f60b14b80de17d52",
   measurementId: "G-VFP8YSFFC1"
 };
 
-// Inicializar Firebase
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
 const analytics = getAnalytics(app);
